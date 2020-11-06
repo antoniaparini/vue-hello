8352c127fcc8cbba1569be2540cad123cc29a7c2
Antonia P <direiellie@gmail.com>
2020-11-06 16:54:07 +0100

    Update link cdn

--- js/main.js
+++ js/main.js
@@ -20,7 +20,9 @@ BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
       },
       methods: {
           info: function(){
-              console.log(`Il lago Trasimeno è un lago della provincia di Perugia, nella regione Umbria e, con una superficie di 128 km², il più esteso lago dell'Italia centrale, quarto tra i laghi italiani subito dopo il lago di Como; tale estensione si affianca pero' ad una scarsa profondità (media 4,3 m, massima 6 m[2]), che lo fa classificare come un lago di tipo laminare. `);
+              console.log(`Il lago Trasimeno è un lago della provincia di Perugia, nella regione Umbria e,
+              con una superficie di 128 km², il più esteso lago dell'Italia centrale, quarto tra i laghi italiani subito dopo il lago di Como; tale estensione
+              si affianca pero' ad una scarsa profondità (media 4,3 m, massima 6 m[2]), che lo fa classificare come un lago di tipo laminare. `);
           }
       }
 
